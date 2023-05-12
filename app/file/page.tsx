bd6f68a51f37fad09fb1af5c2fd9ebdefcdbb803
DanielWTE <daniel.iuser@icloud.com>
2023-05-12 11:49:29 +0200

    Reworked Frontend

--- app/file/page.tsx
+++ app/file/page.tsx
@@ -51,17 +51,47 @@ function UploadFile() {
       bruteforceSafe: { checked: boolean };
       password: { value: string };
       endToEndEncryption: { checked: boolean };
+      autoDeletion: { value: string };
     };
 
     const submitButton = document.getElementById('submit') as HTMLButtonElement;
     submitButton.disabled = true;
+    submitButton.innerHTML = '<div class="flex items-center"><svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg><span class="text-white">Uploading</span></div>';
 
     const bruteforceSafe = target.bruteforceSafe.checked;
     const password = target.password.value;
     const endToEndEncryption = target.endToEndEncryption.checked;
 
-
+    if (files.length > 0) {
+      const formData = new FormData();
+      formData.append('file', files[0]);
+      formData.append('bruteforceSafe', bruteforceSafe.toString());
+      formData.append('password', password);
+      formData.append('endToEndEncryption', endToEndEncryption.toString());
+      formData.append('autoDeletion', target.autoDeletion.value);
+      
+      try {
+        const response = await fetch('/api/file/upload', {
+          method: 'POST',
+          body: formData,
+        });
+        
+        const data = await response.json();
+        if (response.ok) {
+          setUploadLink(data.link);
+          setUploadCreated(true);
+        } else {
+          alert(data.message);
+        }
+      } catch (err) {
+        console.log(err);
+        alert('An error occurred while uploading the file.');
+      }
+    }
+    submitButton.disabled = false;
+    submitButton.innerHTML = 'Start Upload';
   };
+
 
   return (
     <div className="flex items-center justify-center py-8 px-2 md:min-h-screen">
@@ -139,6 +169,25 @@ function UploadFile() {
                 <label htmlFor="endToEndEncryption" className="text-white">
                   End-to-End Encryption
                 </label>
+              </div>
+              <div className="flex flex-col mt-4">
+                <div className="relative">
+                  <select
+                    name="autoDeletion"
+                    id="autoDeletion"
+                    className="bg-slate-950 text-white rounded-lg block p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 focus:shadow-md focus:shadow-blue-700 transition duration-200"
+                  >
+                    <option selected disabled hidden value="download">Choose Auto Deletion</option>
+                    <option value="download">1 Download</option>
+                    <option value="1d">1 Day</option>
+                    <option value="1w">1 Week</option>
+                    <option value="1m">1 Month</option>
+                    <option value="3m">3 Months</option>
+                    <option value="6m">6 Months</option>
+                    <option value="1y">1 Year</option>
+                    <option value="never">Never</option>
+                  </select>
+                </div>
               </div>
               <div className="mt-4">
                 <span className="text-slate-300">Example Link:</span>
