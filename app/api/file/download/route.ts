0a6e0d0df5ff74377fb107b72edf80ed26d43751
DanielWTE <daniel.iuser@icloud.com>
2023-05-12 23:43:34 +0200

    Added Client Downloading API

---
+++ app/api/file/download/route.ts
@@ -0,0 +1,35 @@
+import dbConnect from "@/utils/dbConn";
+import { NextResponse } from "next/server";
+import { findOneDocument } from "@/utils/dbUtils";
+import File from "@/models/file";
+import fs from "fs";
+
+export async function POST(request: Request) {
+  try {
+    const res = await request.json();
+    await dbConnect();
+    let id = res.id;
+    let secret_key = res.secret || null;
+
+    if (!id) {
+      return NextResponse.json({ message: "No file ID provided" }, { status: 400 });
+    }
+
+    const file = await findOneDocument(File, { file_id: id });
+
+    if (file) {
+      const file_path = file.raw_file_path;
+      const file_name = file.file_name;
+
+      // read the file content as binary data
+      const fileData = await fs.promises.readFile(file_path);
+      const fileDataEncoded = Buffer.from(fileData).toString('base64');
+
+      return NextResponse.json({ fileData: fileDataEncoded, secretKey: secret_key, file_name: file_name }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: "No file found with this ID" }, { status: 404 });
+    }
+  } catch (err) {
+    return NextResponse.json({ message: "An error occurred while retrieving the file, please check if the link is correct and try again" }, { status: 500 });
+  }
+};
