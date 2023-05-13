1315450df43827bde02f688f6ca579dca5de4a9c
DanielWTE <daniel.iuser@icloud.com>
2023-05-13 21:31:42 +0200

    Added more seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,26 @@ config.autoAddCss = false
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'Solun • Privacy at its highest',
   description: 'Solun is a service that allows you to share files, text and sending emails with end-to-end encryption, without storing any user related data on our servers. Become anonymous and protect your privacy today.',
   tags: ['Solun', 'Privacy', 'Anonymous', 'Encryption', 'Files', 'Text', 'Emails', 'Share Files', 'Upload Files', 'Send Encrypted Texts'],
+  openGraph: {
+    title: 'Solun • Privacy at its highest',
+    description: 'Solun is a service that allows you to share files, text and sending emails with end-to-end encryption, without storing any user related data on our servers. Become anonymous and protect your privacy today.',
+    url: 'https://solun.pm',
+    siteName: 'Solun',
+    images: [
+      {
+        url: '/solun-logo.png',
+        width: 512,
+        height: 512,
+        alt: 'Solun Logo',
+      },
+    ],
+  },
+  locale: 'en_US',
+  type: 'website',
 }
 
 export default function RootLayout({
