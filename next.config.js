541ef85ba0fdb2900242cdcd6acc07cc915bf2ec
DanielWTE <daniel.iuser@icloud.com>
2023-05-14 23:40:23 +0200

    Added Markdown

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
-    experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
+const nextConfig = {
+    experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"], mdxRs: true, },
     webpack(config) {
         config.experiments = { ...config.experiments, topLevelAwait: true };
         return config;
     }
 };
+
+const withMDX = require('@next/mdx')();
+module.exports = withMDX(nextConfig);
