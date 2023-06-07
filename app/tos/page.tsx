bd0be6e998329ba3d5c6b40bdde57f8e860b6179
schr0der <jannik.schroeder05@gmail.com>
2023-06-07 08:26:12 +0200

    Added Terms of Service

---
+++ app/tos/page.tsx
@@ -0,0 +1,28 @@
+import WelcomerBlog from '@/components/welcomerBlog'
+import CTABanner from '@/components/CTABanner'
+import TosContent from '@/content/tos.mdx'
+
+export const metadata = {
+    title: 'Solun • Terms of Service',
+}
+
+export default function Privacy() {
+
+  return (
+    <div className="bg-primary min-h-screen">
+      <section>
+        <WelcomerBlog title="Terms of Service" />
+      </section>
+      <section>
+        <div className="container mx-auto px-4 md:px-20 py-8 md:py-16 h-full flex items-center justify-center">
+            <div className="prose prose-lg prose-invert max-w-4xl">
+                <TosContent />
+            </div>
+        </div>
+      </section>
+      <section>
+        <CTABanner />
+      </section>
+    </div>
+  )
+}
