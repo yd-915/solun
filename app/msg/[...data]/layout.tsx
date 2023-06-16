5f879b75a03a4d0fe134b9c026f381d65e96c55c
Daniel <daniel.iuser@icloud.com>
2023-06-16 23:57:02 +0200

    spelling

--- app/msg/[...data]/layout.tsx
+++ app/msg/[...data]/layout.tsx
@@ -6,7 +6,7 @@ config.autoAddCss = false
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_MAIN_DOMAIN as string),
-  title: 'Solun • You\'ve recevied a message',
+  title: 'Solun • You\'ve received a message',
   description: 'Do you want to share encrypted messages with your friends? Solun is the right place for you. We offer end-to-end encryption for your messages, without storing any user related data on our servers. Become anonymous and protect your privacy today.',
   tags: ['Solun', 'Privacy', 'Anonymous', 'Encryption', 'Files', 'Text', 'Emails', 'Share Files', 'Upload Files', 'Send Encrypted Texts'],
   openGraph: {
